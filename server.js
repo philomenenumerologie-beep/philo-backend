67dc3f4b4cb753c14691b5291a3b35e68fb48fe9
philomenenumerologie-beep <philomene.numerologie@gmail.com>
2025-10-28 00:34:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/test", (req, res) => {
+  res.sendFile(path.join(__dirname, "test.html"));
+});
 const app = express();
 app.use(express.json());
 app.use(cors({
